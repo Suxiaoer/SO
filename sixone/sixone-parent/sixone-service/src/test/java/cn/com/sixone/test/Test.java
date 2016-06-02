68dfd4ca9a518574534d809682ef49ca2df0ead7
project <2440175602@qq.com>
2016-06-03 07:18:05 +0800

    This is Dog

--- sixone/sixone-parent/sixone-service/src/test/java/cn/com/sixone/test/Test.java
+++ sixone/sixone-parent/sixone-service/src/test/java/cn/com/sixone/test/Test.java
@@ -2,6 +2,6 @@ package cn.com.sixone.test;
 
 public class Test {
 	public static void main(String[] args) {
-        System.out.println("Hello World,I love you too");
+        System.out.println("This is dog");
 	}
 }
